213967dca391593277fb8c0c38bd60f60c6dd3a3
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2025-11-11 22:27:43 +0000

    Add second JavaScript file

---
+++ 1-multi_languages.js
@@ -0,0 +1,18 @@
+/*languages ={
+    C : 'C is fun',
+    Python : 'Python is cool',
+    JavaScript : 'JavaScript is amazing'
+};
+for(i - 0; i<5; i ++){
+    console.log('in loop:',i);
+}
+;*/
+
+
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
