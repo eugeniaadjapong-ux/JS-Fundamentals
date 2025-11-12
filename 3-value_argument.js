c600a0e175215ee15c22f4acd0ec2c3128b84875
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2025-11-12 05:45:27 +0000

    Add 3-value_argument.js file

---
+++ 3-value_argument.js
@@ -0,0 +1,8 @@
+const fruits = ['mango', 'banana', 'orange'];
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+    console.log('No argument');
+}else{
+    console.log('Argument found');
+}
