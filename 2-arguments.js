d8138f7e1de6e048a8050b62651becc3a5e20ad0
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2025-11-12 05:27:28 +0000

    Add a JavaScript file named 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const schoolName = process.avrg.slice(2);
+if (schoolName.Length === 0) {
+    console.log('No arguments found');
+}else if (schoolName.Length === 1){
+    console.log('Argument founnd');
+}else{
+    console.log ('Arguments found');
+}
